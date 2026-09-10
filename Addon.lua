ce2ae1827fa7d6fb403a8a14adc1369a71b1a58b
VadimTofan <vadto@dtu.dk>
2026-09-10 09:56:06 +0200

    data: refresh retail patch notes for 2026-09-10

--- Addon.lua
+++ Addon.lua
@@ -1,7 +1,7 @@
 local _, addon = ...
 
 addon.name = "BetterPatchNotes"
-addon.version = "0.2.33"
+addon.version = "0.2.34"
 
 function addon.GetPlayerContext()
     local _, classToken = UnitClass("player")
